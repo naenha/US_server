70340055a0983483a13820392ae7c6da17d6a83f
jihye <asd36384@gmail.com>
2021-10-31 18:20:01 +0900

    Feat(google_login): finish create login user

--- index.js
+++ index.js
@@ -1,81 +1,33 @@
-const express = require('express')
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
-//const session = require('express-session');
-const nunjucks = require('nunjucks');
-//const sequelize = require('sequelize')
-const dotenv = require('dotenv');
-const passport = require('passport');
-const port = 8080
+var express = require("express");
+var app = express();
+var passport = require("passport");
+var session = require("express-session");
+const { sequelize } = require("./models");
 
-dotenv.config();
+app.set("view engine", "ejs");
+app.use(
+  session({ secret: "MySecret", resave: false, saveUninitialized: true })
+);
 
-//router
-const authRouter = require('./routes/auth');
-
-const { sequelize } = require('./models');
-const passportConfig = require('./passport');
-
-const app = express()
-passportConfig(); //패스포트 설정
-app.set('port', process.env.PORT || 8080);
-app.set('view engine', 'html');
-
-nunjucks.configure('views', {
-  express: app,
-  watch: true,
-});
-
-sequelize.sync({ force: false })
+sequelize
+  .sync({ force: false })
   .then(() => {
-    console.log('데이터베이스 연결 성공');
+    console.log("데이터베이스 연결 성공");
   })
   .catch((err) => {
     console.error(err);
   });
 
-
-
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/img', express.static(path.join(__dirname, 'uploads')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Passport setting
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cookieParser(process.env.COOKIE_SECRET));
-/*app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));*/
 
-//app.use('/', pageRouter);
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Routes
+app.use("/", require("./routes/main"));
+app.use("/auth", require("./routes/auth"));
 
-app.use('/auth', authRouter);
-
-app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
+// Port setting
+var port = 8080;
+app.listen(port, function () {
+  console.log("server on! http://localhost:" + port);
 });
-
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
--- routes/auth.js
+++ routes/auth.js
@@ -1,54 +1,25 @@
-const express = require('express');
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-const User = require('../models/user');
+var express = require("express");
+var router = express.Router();
+var passport = require("../config/passport.js");
 
-const router = express.Router();
-
-router.get('/google', function (req, res, next) {// GET /user/google
-    passport.authenticate('google', { scope: ['profile', 'email'] })(req, res, next);
+router.get("/login", function (req, res) {
+  res.render("auth/login");
 });
 
-//구글 로그인 후 자신의 웹사이트로 돌아오게될 주소 (콜백 url)
-router.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
-
-//로그아웃 페이지 : 로그 아웃 처리 + 세션 삭제 + 쿠키 삭제 후 홈으로 리다이렉션
-//passport 패키지로 인해 req.logout()으로 로그아웃 기능 구현 가능
-router.get('/auth/logout',(req,res,next)=>{
-    req.session.destroy((err)=>{
-        if(err) next(err);
-        req.logOut();
-        res.cookie(`connect.sid`,``,{maxAge:0});
-        res.redirect('/');
-    });
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-/*
-//홈페이지 생성 (req.user는 passport의 serialize를 통해 user 정보 저장되어있음)
-app.get('/',(req,res)=>{
-	const temp = getPage('Welcome', 'Welcome to visit...',getBtn(req.user));
-    res.send(temp);
-});
+router.get("/google/callback", passport.authenticate("google"), authSuccess);
 
-router.get('/google',
-  passport.authenticate('google', { scope: ['email','profile'] }));
-
-router.get('/logout', (req, res) => {
-  return res.send(200, "logout")
-});
-
-
-router.get('/google/callback', passport.authenticate('google', {
-  failureRedirect: '/',
-}), (req, res) => {
-  res.status(200).redirect('/');
-});
-*/
+function authSuccess(req, res) {
+  res.redirect("http://localhost:3000/");
+}
 
 module.exports = router;
