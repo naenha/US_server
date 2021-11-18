3d393f196d245da016cce0bd07d5351247284246
daon <daon318@gmail.com>
2021-11-19 01:13:25 +0900

    share.js

--- routes/share.js
+++ routes/share.js
@@ -39,26 +39,48 @@ router.post('/', async (req, res) => {
 
 
 //나에게 공유된 일기 보기(특정 날짜)
+//미완!!!!
 router.get('/', async (req, res) => {
 
     try {
         const date = req.query.date;
-        const diary = await Share.findAll(
+        const share = await Share.findAll(
             {
                 where: { [Op.and]: [{getting_user: req.query.user_id}, {date: date}]},
                 attributes: ["sending_user", "date"]
             });
         
-        if (diary.length != 0 ) {
-            for (var i = 0; i < diary.length; i++){
-                const conv = await Chat.findOne({
-                    where: { [Op.and]: [{id: diary[i].sending_user}, {date: diary[i].date}]},
-                    attributes: ["sending_user", "date"]
+        //내 아이디와 해당날짜에 공유된 일기가 있다면
+        if (share.length != 0) {
+
+            const name_list = [];
+            const chat_list = [];
+            for (var i = 0; i < share.length; i++){
+                var name = await User.findOne({
+                    where: { id: share[i].sending_user },
+                    attributes: ["nickname"]
                 })
+                name_list.push(name);
+
+                var chat = await Chat.findAll({
+                    where: { user_id: share[i].sending_user },
+                })
+                //var new_date = moment(chat[i].createdAt).format('YYYYMMDD');
+                //console.log(chat.length);
+                //console.log(chat[i].createdAt)
+                for (var j = 0; j < chat.length; j++){
+                    if (moment(chat.createdAt).format('YYYYMMDD') === date){
+                        chat_list.push(chat);
+                    }
+                }
             }
+
+            res.send(chat);
+            
+            
         }
         else {
-            res.send("no diary")
+            res.send("no shared diary")
         }
                 
 
